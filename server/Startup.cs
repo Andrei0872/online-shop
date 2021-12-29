d2e2e8919e939024d01d6f408eae1f39805e4f50
Andrei Gatej <andreigtj01@gmail.com>
2021-12-29 20:05:26 +0200

    chore: apply seed data & configure app

--- server/Startup.cs
+++ server/Startup.cs
@@ -8,6 +8,17 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using server.Data;
+using server.Repositories.ProductRepository;
+using server.Seed;
+using server.Models;
+using server.Helpers.Constants;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Identity;
+
+using server.DAL.UnitOfWork;
+
 namespace server
 {
     public class Startup
@@ -16,10 +27,35 @@ namespace server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ServerContext>(
+                opts => opts.UseSqlServer("Server=localhost;Database=online_shop;User Id=sa;Password=Foo_bar123")
+            );
+
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<ServerContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(UserRoleType.Admin, policy => policy.RequireRole(UserRoleType.Admin));
+                options.AddPolicy(UserRoleType.User, policy => policy.RequireRole(UserRoleType.User));
+            });
+
+            services.Configure<IdentityOptions>(opts => {
+                // Using lenient constraints for demo purposes.
+                opts.Password.RequiredLength = 3;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireUppercase = false;
+            });
+
+            services.AddControllers();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<SeedDb>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, SeedDb seed, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -28,13 +64,20 @@ namespace server
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
+
+            try {
+                seed.SeedRoles().Wait();
+                seed.SeedUsers().Wait();
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
         }
     }
 }
