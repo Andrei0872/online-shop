9da0dd2dc5150caf7e26e4c967609c9b0a277f68
Andrei Gatej <andreigtj01@gmail.com>
2021-12-29 19:53:40 +0200

    feat(role): add model

---
+++ server/Models/Role.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace server.Models
+{
+    public class Role : IdentityRole<int>
+    {
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
