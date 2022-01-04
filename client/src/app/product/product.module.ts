9a787125998da4e12b6056392d7f11227067b83a
Andrei Gatej <andreigtj01@gmail.com>
2022-01-04 15:03:46 +0200

    feat(product): ensure only digits are used for some fields when updating/creating

--- client/src/app/product/product.module.ts
+++ client/src/app/product/product.module.ts
@@ -5,17 +5,19 @@ import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
 import { SingleProductComponent } from './single-product/single-product.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductComponent,
-    SingleProductComponent
+    SingleProductComponent,
   ],
   imports: [
     CommonModule,
     ProductRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
   ]
 })
 export class ProductModule { }
